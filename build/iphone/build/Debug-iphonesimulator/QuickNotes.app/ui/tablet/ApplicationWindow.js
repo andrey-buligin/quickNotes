aebeffbb223ed65aae7a41cc46ff9ffe652a5c89
Andrejs Buligins <buligin.developer@gmail.com>
2012-12-15 16:43:20 +0000

    Added alloy framework

--- build/iphone/build/Debug-iphonesimulator/QuickNotes.app/ui/tablet/ApplicationWindow.js
+++
@@ -1,1 +0,0 @@
-/Users/andrey/Sites/Titanium_Studio_Workspace/QuickNotes/Resources/ui/tablet/ApplicationWindow.js
--- build/iphone/main.m
+++ build/iphone/main.m
@@ -9,12 +9,12 @@
 
 NSString * const TI_APPLICATION_DEPLOYTYPE = @"development";
 NSString * const TI_APPLICATION_ID = @"com.andreybuligin.quickNotes";
-NSString * const TI_APPLICATION_PUBLISHER = @"andrey buligin";
+NSString * const TI_APPLICATION_PUBLISHER = @"Andrey Buligin";
 NSString * const TI_APPLICATION_URL = @"http://www.andreybuligin.com";
 NSString * const TI_APPLICATION_NAME = @"QuickNotes";
 NSString * const TI_APPLICATION_VERSION = @"1.0";
 NSString * const TI_APPLICATION_DESCRIPTION = @"QuickNotes allows to create, manage and search your existing notes in an easy manner using swipe gestures.";
-NSString * const TI_APPLICATION_COPYRIGHT = @"2012 by andrey buligin";
+NSString * const TI_APPLICATION_COPYRIGHT = @"2012 by Andrey Buligin";
 NSString * const TI_APPLICATION_GUID = @"5aa0ab22-f804-4704-b3ae-abf4128e3f72";
 BOOL const TI_APPLICATION_ANALYTICS = true;
 
